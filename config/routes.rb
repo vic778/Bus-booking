b69dbd66a5a1ec29042f945d2d984161f5398b83
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-28 12:07:17 +0300

    Install gems

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  root 'pages#home'
+  devise_for :users
+  
+  get 'user/auto_login', to: 'users#auto_login'
+ 
+end
