ab2fa752a228dd75adde753724b01df7d38e48ee
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-28 12:23:04 +0300

    Migrate agence model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_28_071106) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_092121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_071106) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "agences", force: :cascade do |t|
+    t.string "name"
+    t.integer "register_number"
+    t.string "road"
+    t.string "avenue"
+    t.string "contact"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
