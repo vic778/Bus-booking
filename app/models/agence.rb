eaa786a0110d6726e8ceaa84ef5082c4d7dea5f3
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-28 12:36:52 +0300

    Validate presnece of values

--- app/models/agence.rb
+++ app/models/agence.rb
@@ -1,2 +1,7 @@
 class Agence < ApplicationRecord
+    belongs_to :user
+
+    validates :name, presence: true, uniqueness: true
+    validates :register_number, presence: true, uniqueness: true
+    validates :road, presence: true
 end
