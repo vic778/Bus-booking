c316b1188e2b5507cf451ee264782a2c0b05a627
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-28 18:01:59 +0300

    Fix linyters error

--- spec/factories/agences.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :agence do
-    name { "MyString" }
-    register_number { 1 }
-    road { "MyString" }
-    avenue { "MyString" }
-  end
-end
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,10 +1,17 @@
 FactoryBot.define do
-    factory :user do
-        first_name { Faker::Name.first_name }
-        last_name { Faker::Name.last_name }
-        phone_number { Faker::PhoneNumber.phone_number }
-        email { Faker::Internet.email }
-        password { Faker::Internet.password }
-        password_confirmation { password }
-    end
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone_number { Faker::PhoneNumber.phone_number }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    password_confirmation { password }
+  end
+
+  factory :agence do
+    name { "MyString" }
+    register_number { 1 }
+    road { "MyString" }
+    avenue { "MyString" }
+  end
 end
