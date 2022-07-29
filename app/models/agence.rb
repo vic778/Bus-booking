03ed4374993b18da24733c37f68352d657341ffe
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-29 18:57:23 +0300

    Associate to bus

--- app/models/agence.rb
+++ app/models/agence.rb
@@ -1,5 +1,6 @@
 class Agence < ApplicationRecord
   belongs_to :user
+  has_many :buses
 
   validates :name, presence: true, uniqueness: true
   validates :register_number, presence: true, uniqueness: true
