f2be5278f6f2a13a95612dea5c4dd199cab260b4
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-30 22:10:30 +0300

    Fix linters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_153720) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_143356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_153720) do
     t.bigint "agence_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "time"
     t.index ["agence_id"], name: "index_buses_on_agence_id"
   end
 
