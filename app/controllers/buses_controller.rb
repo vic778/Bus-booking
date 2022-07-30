72b8069b5cbec7323b3de536747d138b3a5c17d2
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-30 17:26:13 +0300

    Create a bus and merge it to agence

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,2 +1,37 @@
 class BusesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_bus, only: [:show, :edit, :update, :destroy]
+
+    def create
+        @bus = Bus.create(bus_params.merge(agence_id: current_user.agence.id))
+        if @bus.save
+            redirect_to root_path, notice: "Bus created successfully"
+        else
+            redirect_to root_path, alert: "Bus not created"
+        end
+    end
+
+    def update
+        if @bus.update(bus_params)
+            redirect_to root_path, notice: "Bus updated successfully"
+        else
+            redirect_to root_path, alert: "Bus not updated"
+        end
+    end
+
+    def destroy
+        @bus.destroy
+        redirect_to root_path, notice: "Bus deleted successfully"
+    end
+
+    private
+
+    def set_bus
+        @bus = Bus.find(params[:id])
+    end
+
+    def bus_params
+        params.require(:bus).permit(:name, :register_number, :road, :avenue, :contact, :agence_id)
+    end
+    
 end
